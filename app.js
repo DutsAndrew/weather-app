d85253488de9bbb590b61400e079b6ff642b373f
Andrew Dutson <Dutsandrew@gmail.com>
2022-07-03 20:00:02 -0600

    api calls are appended to dom with weather information

--- app.js
+++ app.js
@@ -40,7 +40,26 @@ async function getTodaysWeather() {
   try {
     const response = await fetch (searchWeather, {mode: 'cors'});
     const searchData = await response.json();
-    console.log(searchData);
+    
+    // variables for information to be appended to the DOM for weather display
+    let temp = searchData.main.temp;
+    let weatherType = searchData.weather[0].main;
+    let description = searchData.weather[0].description;
+    let country = searchData.sys.country;
+    let feelsLike = searchData.main.feels_like;
+    let humidity = searchData.main.humidity;
+    let tempMin = searchData.main.temp_min;
+    let tempMax = searchData.main.temp_max;
+    appendCurrentWeather(
+      temp,
+      weatherType,
+      description,
+      country,
+      feelsLike,
+      humidity,
+      tempMin,
+      tempMax
+    );
   } catch (error) {
     console.log(error);
     alert('The server could not find what you were looking for, please try again');
@@ -64,4 +83,49 @@ async function getWeatherForecast() {
     console.log(error);
     alert('The server could not find what you were looking for, please try again');
   }
+}
+
+function appendCurrentWeather(
+  temp,
+  weatherType,
+  description,
+  country,
+  feelsLike,
+  humidity,
+  tempMin,
+  tempMax
+  ) {
+    const locationInformation = document.querySelector('#location-information');
+    let city = document.createElement('p');
+      city.setAttribute('id', 'city-name');
+      city.textContent = `${retrievedCityName}, ${country}`;
+    let weatherDescription = document.createElement('p');
+      weatherDescription.setAttribute('id', 'weather-description');
+      weatherDescription.textContent = `${weatherType}, ${description}`;
+    let weatherTemperature = document.createElement('p');
+      weatherTemperature.setAttribute('id', 'weather-temperature');
+      weatherTemperature.textContent = `${temp} °F`;
+
+    locationInformation.appendChild(city);
+    locationInformation.appendChild(weatherDescription);
+    locationInformation.appendChild(weatherTemperature);
+
+    const locationExtraInformation = document.querySelector('#location-extra-information');
+    let weatherFeelsLike = document.createElement('p');
+      weatherFeelsLike.setAttribute('id', 'weather-feels-like');
+      weatherFeelsLike.textContent = `Feels Like: ${feelsLike}`;
+    let weatherHumidity = document.createElement('p');
+      weatherHumidity.setAttribute('id', 'weather-humidity');
+      weatherHumidity.textContent = `Humidity: ${humidity}`;
+    let weatherMin = document.createElement('p');
+      weatherMin.setAttribute('id', 'weather-min');
+      weatherMin.textContent = `Temperature Low: ${tempMin}`;
+    let weatherMax = document.createElement('p');
+      weatherMax.setAttribute('id', 'weather-max');
+      weatherMax.textContent = `Temperature High: ${tempMax}`;
+
+    locationExtraInformation.appendChild(weatherFeelsLike);
+    locationExtraInformation.appendChild(weatherHumidity);
+    locationExtraInformation.appendChild(weatherMin);
+    locationExtraInformation.appendChild(weatherMax);
 }
